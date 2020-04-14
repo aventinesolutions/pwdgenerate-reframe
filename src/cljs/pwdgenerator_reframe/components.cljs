18a6a1969f91552639dd0970c292e834ce566d37
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-04-14 20:51:08 +0200

    :bear: :construction: add the login form to the "personalized params" card
    #172315027 As a User, I want to be able to store my personalized params so that I
     can use then later.

--- src/cljs/pwdgenerator_reframe/components.cljs
+++ src/cljs/pwdgenerator_reframe/components.cljs
@@ -9,4 +9,8 @@
   ^{:key :save-personalized-params}
   [:div {:id :save-personalized-params
          :class card-classes}
-   [:h4.uk-text-primary "save personalized params"]])
+   [:h4.uk-text-primary "Personalized Params" [:span {:uk-icon "icon: check"}]]
+   [:fieldset.uk-form-stacked.uk-padding
+    [:label.text-primary "email address" [:input.uk-input {:type :text}]]
+    [:label.text-primary "password" [:input.uk-input {:type :password}]]
+    [:button.uk-button-primary "login"]]])
