2a5eb72f558dfcfeecf7fdc1eb49d1c7ce83c0cc
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-04-23 17:11:28 +0200

    :bear: :construction: make the Firebase login it's own component
    #172315027 As a User, I want to be able to store my personalized params so that I
     can use then later.

--- src/cljs/pwdgenerator_reframe/components/save_personalized_params.cljs
+++ src/cljs/pwdgenerator_reframe/components/save_personalized_params.cljs
@@ -4,15 +4,11 @@
     [re-frame.core :as re-frame]
     [pwdgenerator-reframe.styles :refer [card-classes]]
     [pwdgenerator-reframe.subs :as subs]
-    [pwdgenerator-reframe.firebase :as firebase]))
-
-(defonce component-state (atom {}))
+    [pwdgenerator-reframe.components.login :refer [login]]))
 
 (defn save-personalized-params []
   (fn []
-    (let [user (re-frame/subscribe [::subs/user])
-          email (:email @component-state)
-          password (:password @component-state)]
+    (let [user (re-frame/subscribe [::subs/user])]
       [:div
        {:id    :save-personalized-params
         :class card-classes}
@@ -20,30 +16,4 @@
         [:span.uk-icon {:data-uk-icon "icon: heart"}]
         " Personalized Params "
         [:span.uk-text-small (:email @user)]]
-       [:fieldset.uk-form-stacked.uk-padding
-        {:on-focus
-         (fn [event]
-           (do
-             (.preventDefault event)
-             (firebase/init)))}
-        [:label.text-primary "email address"
-         [:input.uk-input {:type          :text
-                           :auto-complete :email
-
-                           :on-change     (fn [event]
-                                            (do
-                                              (.preventDefault event)
-                                              (swap! component-state assoc :email (-> event .-target .-value))))}]]
-        [:label.text-primary "password"
-         [:input.uk-input {:type          :password
-                           :auto-complete :current-password
-                           :on-change     (fn [event]
-                                            (do
-                                              (.preventDefault event)
-                                              (swap! component-state assoc :password (-> event .-target .-value))))}]]
-        [:button {:class [:uk-button :uk-button-primary :uk-button-small]
-                  :on-click
-                         (fn [event]
-                           (.preventDefault event)
-                           (re-frame/dispatch [::firebase/sign-in-by-email [email password]]))}
-         "login"]]])))
+       (if (nil? @user) [login])])))
